ec1e522e8cc051ef8695821a94edfcea8dc515be
Vladimir Piskarev <pisv@1c.ru>
2022-03-14 20:24:48 +0300

    Support multiple providers for document links in LX4E
    Multiple document link providers can be registered for a language. In
    that case providers are asked in parallel and the results are merged. A
    failing provider will not cause a failure of the whole operation.

--- org.lxtk.lx4e.ui/src/org/lxtk/lx4e/ui/hyperlinks/DocumentHyperlinkDetector.java
+++ org.lxtk.lx4e.ui/src/org/lxtk/lx4e/ui/hyperlinks/DocumentHyperlinkDetector.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021 1C-Soft LLC.
+ * Copyright (c) 2021, 2022 1C-Soft LLC.
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which is available at
@@ -12,10 +12,12 @@
  *******************************************************************************/
 package org.lxtk.lx4e.ui.hyperlinks;
 
-import java.net.URI;
 import java.time.Duration;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
@@ -28,13 +30,15 @@ import org.lxtk.DocumentLinkProvider;
 import org.lxtk.DocumentUri;
 import org.lxtk.LanguageOperationTarget;
 import org.lxtk.LanguageService;
+import org.lxtk.jsonrpc.JsonUtil;
 import org.lxtk.lx4e.DocumentUtil;
+import org.lxtk.lx4e.internal.ui.TaskExecutor;
 import org.lxtk.lx4e.requests.DocumentLinkRequest;
 import org.lxtk.lx4e.ui.WorkDoneProgressFactory;
 
 /**
  * Default implementation of a hyperlink detector that computes hyperlinks
- * using a {@link DocumentLinkProvider}.
+ * using {@link DocumentLinkProvider}s.
  */
 public class DocumentHyperlinkDetector
     extends AbstractHyperlinkDetector
@@ -43,50 +47,131 @@ public class DocumentHyperlinkDetector
     public IHyperlink[] detectHyperlinks(ITextViewer textViewer, IRegion region,
         boolean canShowMultipleHyperlinks)
     {
-        LanguageOperationTarget target = getAdapter(LanguageOperationTarget.class);
+        LanguageOperationTarget target = getLanguageOperationTarget();
         if (target == null)
             return null;
 
-        URI documentUri = target.getDocumentUri();
-        LanguageService languageService = target.getLanguageService();
-        DocumentLinkProvider provider = languageService.getDocumentMatcher().getBestMatch(
-            languageService.getDocumentLinkProviders(), DocumentLinkProvider::getDocumentSelector,
-            documentUri, target.getLanguageId());
-        if (provider == null)
+        DocumentLinkResults results = computeDocumentLinkResults(getDocumentLinkProviders(target),
+            new DocumentLinkParams(DocumentUri.toTextDocumentIdentifier(target.getDocumentUri())));
+        if (results == null)
             return null;
 
+        return createHyperlinks(results, textViewer, region, canShowMultipleHyperlinks);
+    }
+
+    /**
+     * Returns the current {@link LanguageOperationTarget}.
+     *
+     * @return the language operation target, or <code>null</code> if none
+     */
+    public LanguageOperationTarget getLanguageOperationTarget()
+    {
+        return getAdapter(LanguageOperationTarget.class);
+    }
+
+    /**
+     * Returns document link providers that match the given target.
+     *
+     * @param target never <code>null</code>
+     * @return the matching document link providers (not <code>null</code>)
+     */
+    protected DocumentLinkProvider[] getDocumentLinkProviders(LanguageOperationTarget target)
+    {
+        LanguageService languageService = target.getLanguageService();
+        return languageService.getDocumentMatcher().getSortedMatches(
+            languageService.getDocumentLinkProviders(), DocumentLinkProvider::getDocumentSelector,
+            target.getDocumentUri(), target.getLanguageId()).toArray(DocumentLinkProvider[]::new);
+    }
+
+    /**
+     * Asks each of the given document link providers to compute a result for the given
+     * {@link DocumentLinkParams} and returns the computed results.
+     *
+     * @param providers never <code>null</code>
+     * @param params never <code>null</code>
+     * @return the computed document link results, or <code>null</code> if none
+     */
+    protected DocumentLinkResults computeDocumentLinkResults(DocumentLinkProvider[] providers,
+        DocumentLinkParams params)
+    {
+        if (providers.length == 0)
+            return null;
+
+        return new DocumentLinkResults(TaskExecutor.parallelCompute(providers,
+            (provider, monitor) -> computeDocumentLinkResult(provider, params, monitor),
+            Messages.Computing_links, getDocumentLinkTimeout(), null));
+    }
+
+    /**
+     * Asks the given document link provider to compute a result for the given
+     * {@link DocumentLinkParams} and returns the computed result.
+     *
+     * @param provider never <code>null</code>
+     * @param params never <code>null</code>
+     * @param monitor a progress monitor, or <code>null</code>
+     *  if progress reporting is not desired. The caller must not rely on
+     *  {@link IProgressMonitor#done()} having been called by the receiver
+     * @return the computed document link result, or <code>null</code> if none
+     */
+    protected DocumentLinkResult computeDocumentLinkResult(DocumentLinkProvider provider,
+        DocumentLinkParams params, IProgressMonitor monitor)
+    {
         DocumentLinkRequest request = newDocumentLinkRequest();
         request.setProvider(provider);
-        request.setParams(
-            new DocumentLinkParams(DocumentUri.toTextDocumentIdentifier(documentUri)));
-        request.setUpWorkDoneProgress(
-            () -> WorkDoneProgressFactory.newWorkDoneProgressWithJob(false));
+        // note that request params can get modified as part of request processing
+        // (e.g. a progress token can be set); therefore we need to copy the given params
+        request.setParams(JsonUtil.deepCopy(params));
+        request.setProgressMonitor(monitor);
+        request.setUpWorkDoneProgress(WorkDoneProgressFactory::newWorkDoneProgress);
         request.setTimeout(getDocumentLinkTimeout());
         request.setMayThrow(false);
 
-        List<DocumentLink> result = request.sendAndReceive();
-        if (result == null)
-            return null;
-
-        return createHyperlinks(textViewer, region, canShowMultipleHyperlinks, provider, result);
+        return new DocumentLinkResult(request.sendAndReceive());
     }
 
-    private IHyperlink[] createHyperlinks(ITextViewer textViewer, IRegion region,
-        boolean canShowMultipleHyperlinks, DocumentLinkProvider provider, List<DocumentLink> links)
+    /**
+     * Creates and returns hyperlinks for the given document link results that have been computed
+     * for the given region in the given text viewer.
+     *
+     * @param results never <code>null</code>
+     * @param textViewer the text viewer on which the hover popup should be shown
+     *  (never <code>null</code>)
+     * @param region the hyperlink region (never <code>null</code>)
+     * @param canShowMultipleHyperlinks tells whether the caller is able to show multiple hyperlinks
+     * @return the created hyperlinks, or <code>null</code> if none
+     */
+    protected IHyperlink[] createHyperlinks(DocumentLinkResults results, ITextViewer textViewer,
+        IRegion region, boolean canShowMultipleHyperlinks)
     {
-        for (DocumentLink link : links)
+        for (Map.Entry<DocumentLinkProvider, DocumentLinkResult> entry : results.asMap().entrySet())
         {
-            IRegion linkRegion;
-            try
+            DocumentLinkResult result = entry.getValue();
+            if (result != null)
             {
-                linkRegion = DocumentUtil.toRegion(textViewer.getDocument(), link.getRange());
+                List<DocumentLink> links = result.getDocumentLinks();
+                if (links != null)
+                {
+                    for (DocumentLink link : links)
+                    {
+                        IRegion linkRegion;
+                        try
+                        {
+                            linkRegion =
+                                DocumentUtil.toRegion(textViewer.getDocument(), link.getRange());
+                        }
+                        catch (BadLocationException e)
+                        {
+                            continue;
+                        }
+
+                        if (TextUtilities.overlaps(region, linkRegion))
+                        {
+                            return new IHyperlink[] {
+                                createHyperlink(linkRegion, link, entry.getKey()) };
+                        }
+                    }
+                }
             }
-            catch (BadLocationException e)
-            {
-                continue;
-            }
-            if (TextUtilities.overlaps(region, linkRegion))
-                return new IHyperlink[] { createHyperlink(linkRegion, link, provider) };
         }
         return null;
     }
@@ -124,5 +209,61 @@ public class DocumentHyperlinkDetector
     protected Duration getDocumentLinkTimeout()
     {
         return Duration.ofSeconds(1);
+    }
+
+    /**
+     * Represents a group of {@link DocumentLinkResult}s.
+     */
+    protected static class DocumentLinkResults
+    {
+        private final Map<DocumentLinkProvider, DocumentLinkResult> results;
+
+        /**
+         * Constructor.
+         *
+         * @param results not <code>null</code>
+         */
+        public DocumentLinkResults(Map<DocumentLinkProvider, DocumentLinkResult> results)
+        {
+            this.results = Objects.requireNonNull(results);
+        }
+
+        /**
+         * Returns the document link results as a map.
+         *
+         * @return the document link results as a map (never <code>null</code>)
+         */
+        public Map<DocumentLinkProvider, DocumentLinkResult> asMap()
+        {
+            return results;
+        }
+    }
+
+    /**
+     * Represents the result of a document link request.
+     */
+    protected static class DocumentLinkResult
+    {
+        private final List<DocumentLink> links;
+
+        /**
+         * Constructor.
+         *
+         * @param links may be <code>null</code>
+         */
+        public DocumentLinkResult(List<DocumentLink> links)
+        {
+            this.links = links;
+        }
+
+        /**
+         * Returns the document link list.
+         *
+         * @return the document link list, or <code>null</code> if none
+         */
+        public List<DocumentLink> getDocumentLinks()
+        {
+            return links;
+        }
     }
 }
